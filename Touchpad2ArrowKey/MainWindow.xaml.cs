e22a4ea26cdb6bddcf638f21d2a73bef191414d2
asdf49514 <dkswnstj021@gmail.com>
2022-03-13 01:25:03 +0900

    활성화 조건을 키다운 -> 토글로 변경 마우스 커서 이동속도에 비례하여 텍스트 커서가 이동하도록 함 각도 범위(가로, 세로) 조절 기능 추가 민감도 조절 기능 추가

--- Touchpad2ArrowKey/MainWindow.xaml.cs
+++ Touchpad2ArrowKey/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Forms;
+using System.Windows.Threading;
+using Extensions;
 using WindowsInput;
 using WindowsInput.Native;
 using Point = System.Drawing.Point;
+using Timer = System.Timers.Timer;
 
 namespace Touchpad2ArrowKey
 {
@@ -13,6 +18,9 @@ namespace Touchpad2ArrowKey
 	/// </summary>
 	public partial class MainWindow
 	{
+		private bool firstActivateAfterExecution;
+		private bool isActivated;
+		
 		private Point lastPos;
 		private bool lastPosHasValue;
 		
@@ -20,31 +28,58 @@ namespace Touchpad2ArrowKey
 		private double vecAngle;  //Degree
 		private double vecLength;
 
-		private double sensivity;
-		private double minVecLength;
-		private double angleSensiv;
+		private double sensitivity;
+		private int threadSleepTime;
+		private double angleSensitivLR;
+		private double angleSensitivUD;
+		
+		//Options
+		private bool freezeCursor;
+		private Point cursorFreezePos;
 
 		private bool isDebugging;
 		
 		private readonly InputSimulator inputSim;
+		private Thread t2AThread;
+		
+		private Timer activationTimer;
+		
 		public MainWindow()
 		{
-			lastPos = new Point(0, 0);  //임시값
+			firstActivateAfterExecution = false;
+			isActivated = false;
+			
+			lastPos = new Point();  //임시값
 			lastPosHasValue = false;
 			
-			deltaVec = new Vector(0, 0);
+			deltaVec = new Vector();
 			vecAngle = 0;
 			vecLength = 0;
 
-			sensivity = 10;
-			minVecLength = Math.Max(1, 100 / sensivity);
-			angleSensiv = 30;
+			sensitivity = 10;
+			threadSleepTime = 3000;
+			angleSensitivLR = 60;
+			angleSensitivUD = 30;
+
+			freezeCursor = false;
+			cursorFreezePos = new Point();
 
 			isDebugging = true;
 
 			inputSim = new InputSimulator();
 
+			activationTimer = new Timer {
+				AutoReset = false, Interval = 750
+			};
+
+
 			InitializeComponent();
+			
+			DebugIsActivated.IsChecked = isActivated;
+			DeactivateButton.IsEnabled = false;
+			//todo Slider, ValueLabel 값 초기화 하기
+			
+				
 
 			Hook.KeyboardHook.KeyDown += KeyboardHook_KeyDown;
 			Hook.KeyboardHook.KeyUp += KeyboardHook_KeyUp;
@@ -57,56 +92,129 @@ namespace Touchpad2ArrowKey
 		
 		private bool KeyboardHook_KeyDown(int vkCode) {
 			DebuggingLogTextBox.AppendText($"{vkCode}|");
-			if(Keyboard.Modifiers == ModifierKeys.Shift)  //todo 토글 방식으로 변경하기
-				TouchPadToArrowKey();
+
+			if(vkCode.ToString() != "164") return true;
+			
+			if(activationTimer.Enabled) {
+				activationTimer.Stop();
+
+				if(isActivated)
+					T2ADeactivate();
+				else
+					T2AActivate();
+			}
+			else { activationTimer.Start(); }
+
 			
 			return true;
+		}
+
+		private static bool KeyboardHook_KeyUp(int vkCode) {
+			return true;
+		}
+		
+
+		private void ActivateButton_Click(object sender, RoutedEventArgs e)
+			=> T2AActivate();
+
+		private void DeactivateButton_Click(object sender, RoutedEventArgs e) 
+			=> T2ADeactivate();
+		
+		private void T2AActivate()
+		{
+			isActivated = true;
+			DebugIsActivated.IsChecked = isActivated;
+			ActivateButton.IsEnabled = false;
+			DeactivateButton.IsEnabled = true;
+
+			AngleSensitivitySlider.IsEnabled = false;
+			SensitivitySlider.IsEnabled = false;
+			//FreezeCursorCheckbox.IsEnabled = false;
+			
+			cursorFreezePos = System.Windows.Forms.Cursor.Position;
+			
+			t2AThread = new Thread(TouchPadToArrowKey) {
+				IsBackground = true
+			};
+			t2AThread.Start();
+
+			if(firstActivateAfterExecution) {
+				Thread.Sleep(200);
+				T2ADeactivate();
+				T2AActivate();
+				firstActivateAfterExecution = false;
+			}
+		}
+		
+		private void T2ADeactivate()
+		{
+			isActivated = false;
+			DebugIsActivated.IsChecked = isActivated;
+			DeactivateButton.IsEnabled = false;
+			ActivateButton.IsEnabled = true;
+			
+			AngleSensitivitySlider.IsEnabled = true;
+			SensitivitySlider.IsEnabled = true;
+			//FreezeCursorCheckbox.IsEnabled = true;
+
+			t2AThread.Join();
 		}
 		
 		private void TouchPadToArrowKey()
 		{
-			var curPos = System.Windows.Forms.Cursor.Position;
+			while(isActivated) {
+				var curPos = System.Windows.Forms.Cursor.Position;
 
-			if(!lastPosHasValue) {
+				if(!lastPosHasValue) {
+					lastPos = curPos;
+					lastPosHasValue = true;
+					return;
+				}
+
+				SetDeltaVector(curPos, lastPos);
 				lastPos = curPos;
-				lastPosHasValue = true;
-				return;
-			}
 
-			SetDeltaVector(curPos, lastPos);
-			lastPos = curPos;
+				vecAngle = Math.Atan2(deltaVec.X, deltaVec.Y) * (180 / Math.PI);
+				vecLength = deltaVec.Length;
 
-			vecAngle = Math.Atan2(deltaVec.X, deltaVec.Y) * (180 / Math.PI);
-			vecLength = deltaVec.Length;
-			
-			if(vecLength >= minVecLength) {
-				if((0 - angleSensiv) <= vecAngle && vecAngle <= (0 + angleSensiv)) 
-					inputSim.Keyboard.KeyDown(VirtualKeyCode.DOWN);
-				else if((90 - angleSensiv) <= vecAngle && vecAngle <= (90 + angleSensiv)) 
-					inputSim.Keyboard.KeyDown(VirtualKeyCode.RIGHT);
-				else if(!((-180 + angleSensiv) <= vecAngle && vecAngle <= (180 - angleSensiv))) 
-					inputSim.Keyboard.KeyDown(VirtualKeyCode.UP);
-				else if((-90 - angleSensiv) <= vecAngle && vecAngle <= (-90 + angleSensiv)) 
-					inputSim.Keyboard.KeyDown(VirtualKeyCode.LEFT);
-			}
+				if(deltaVec.Length == 0) continue;
 
-			if(isDebugging) {
-				var direction = "NONE";
+				if(deltaVec.Length >= 2) {
+					if((0 - angleSensitivUD) <= vecAngle && vecAngle <= (0 + angleSensitivUD))
+						inputSim.Keyboard.KeyDown(VirtualKeyCode.DOWN);
+					else if((90 - angleSensitivLR) <= vecAngle && vecAngle <= (90 + angleSensitivLR))
+						inputSim.Keyboard.KeyDown(VirtualKeyCode.RIGHT);
+					else if(!((-180 + angleSensitivUD) <= vecAngle && vecAngle <= (180 - angleSensitivUD)))
+						inputSim.Keyboard.KeyDown(VirtualKeyCode.UP);
+					else if((-90 - angleSensitivLR) <= vecAngle && vecAngle <= (-90 + angleSensitivLR))
+						inputSim.Keyboard.KeyDown(VirtualKeyCode.LEFT);
+					
+					Thread.Sleep((int) (threadSleepTime / (deltaVec.Length * sensitivity + 1)));
+				}
 				
-				if((0 - angleSensiv) <= vecAngle && vecAngle <= (0 + angleSensiv))
-					direction = "DOWN";
-				else if((90 - angleSensiv) <= vecAngle && vecAngle <= (90 + angleSensiv))
-					direction = "RIGHT";
-				else if(!((-180 + angleSensiv) <= vecAngle && vecAngle <= (180 - angleSensiv)))
-					direction = "UP";
-				else if((-90 - angleSensiv) <= vecAngle && vecAngle <= (-90 + angleSensiv))
-					direction = "LEFT";
-
-				DisplayDebuggingTexts(curPos, lastPos, deltaVec, vecAngle, direction);
+				// if(freezeCursor)
+				// 	System.Windows.Forms.Cursor.Position = cursorFreezePos;
 			}
 		}
 
+		private void TouchpadToArrowKey_DebuggingLog()
+		{
+			if(isDebugging) {
+				var direction = "NONE";
 
+				if((0 - angleSensitivUD) <= vecAngle && vecAngle <= (0 + angleSensitivUD))
+					direction = "DOWN";
+				else if((90 - angleSensitivUD) <= vecAngle && vecAngle <= (90 + angleSensitivUD))
+					direction = "RIGHT";
+				else if(!((-180 + angleSensitivUD) <= vecAngle && vecAngle <= (180 - angleSensitivUD)))
+					direction = "UP";
+				else if((-90 - angleSensitivUD) <= vecAngle && vecAngle <= (-90 + angleSensitivUD))
+					direction = "LEFT";
+
+				DisplayDebuggingTexts(lastPos, deltaVec, vecAngle, direction);
+			}
+		}
+		
 		private void SetDeltaVector(Point curPosParam, Point lastPosParam)
 		{
 			deltaVec.X = curPosParam.X - lastPosParam.X;
@@ -114,20 +222,38 @@ namespace Touchpad2ArrowKey
 		}
 
 
-		private void DisplayDebuggingTexts(Point curPosParam, Point lastPosParam, Vector deltaVectorParam, double vecAngle, string vecDirection)
+		private void DisplayDebuggingTexts(Point lastPosParam, Vector deltaVectorParam, double vecAngle, string vecDirection)
 		{
-			DebuggingLogTextBox.Text = $"Cursor Position  : {curPosParam.X}, {curPosParam.Y}\n" +
+			DebuggingLogTextBox.Text = $"Cursor Position  : ???, ???\n" +
 			                           $"Last Position    : {lastPosParam.X}, {lastPosParam.Y}\n" +
 			                           $"Delta Vector     : {deltaVec.X}, {deltaVec.Y}, \n" +
 			                           $"                   {deltaVec.Length}\n" +
 			                           $"Vector Angle     : {vecAngle}\n" +
 			                           $"Vector Direction : {vecDirection}\n" +
-			                           $"minVecLength     : {minVecLength}\n" +
-			                           $"Is Simulated     : {vecLength >= minVecLength}";
+			                           $"minVecLength     : {threadSleepTime}\n" +
+			                           $"Is Simulated     : {vecLength >= threadSleepTime}";
 		}
 
-		private static bool KeyboardHook_KeyUp(int vkCode) {
-			return true;
+		private void AngleSensitivitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if(AngleSensitivityValueLabel == null) return;
+			
+			angleSensitivUD = FloorRoundCeil.RoundFrom(AngleSensitivitySlider.Value, 2);
+			AngleSensitivityValueLabel.Content = $"AngleSensitivity: {angleSensitivUD}";
+		}
+
+		private void SensitivitySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if(SensitivityValueLabel == null) return;
+			
+			sensitivity = FloorRoundCeil.RoundFrom(SensitivitySlider.Value, 2);
+			SensitivityValueLabel.Content = $"Sensitivity: {sensitivity}";
+		}
+
+		private void FreezeCursorCheckbox_Clicked(object sender, RoutedEventArgs e)
+		{
+			if(FreezeCursorCheckbox.IsChecked != null)
+				freezeCursor = (bool)FreezeCursorCheckbox.IsChecked;
 		}
 	}
 }
