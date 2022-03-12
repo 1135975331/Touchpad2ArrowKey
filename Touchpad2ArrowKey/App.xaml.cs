4793e367295c08e5a61399bb768b176e02088c50
asdf49514 <dkswnstj021@gmail.com>
2022-03-12 15:43:36 +0900

    Initial commit

--- Touchpad2ArrowKey/App.xaml.cs
+++ Touchpad2ArrowKey/App.xaml.cs
@@ -1,7 +1,18 @@
-﻿namespace Touchpad2ArrowKey
+﻿using System.Windows;
+
+namespace Touchpad2ArrowKey
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App { }
+	public partial class App
+	{
+		public App() : base() {
+			this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+		}
+
+		static void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+			MessageBox.Show("Unhandled exception occurred: \n" + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+	}
 }
