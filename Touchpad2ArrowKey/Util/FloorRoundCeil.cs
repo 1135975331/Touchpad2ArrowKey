bbc90739ffad39975d4ad5a1f6f1c13d7e62e483
asdf49514 <dkswnstj021@gmail.com>
2022-03-13 01:27:01 +0900

    버림/반올림/내림 유틸 코드 파일 추가

---
+++ Touchpad2ArrowKey/Util/FloorRoundCeil.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Extensions
+{
+	public static class FloorRoundCeil
+	{
+		public static float FloorFrom(float value, int pos)
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return (float)(Math.Floor(value*powOf10) / powOf10);
+		}
+
+		public static float RoundFrom(float value, int pos)
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return (float)(Math.Round(value*powOf10) / powOf10);
+		}
+
+		public static float CeilFrom(float value, int pos)		
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return (float)(Math.Ceiling(value*powOf10) / powOf10);
+		}
+		
+		
+		public static double FloorFrom(double value, int pos)
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return Math.Floor((float)value*powOf10) / powOf10;
+		}
+
+		public static double RoundFrom(double value, int pos)
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return Math.Round((float)value*powOf10) / powOf10;
+		}
+
+		public static double CeilFrom(double value, int pos)		
+		{
+			if(pos < 1)
+				throw new ArgumentException("\'pos\' should be at least 1.");
+
+			var powOf10 = Math.Pow(10, pos-1);
+
+			return Math.Ceiling((float)value*powOf10) / powOf10;
+		}
+	}
+}
